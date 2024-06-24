d91f8a3ae0117160ebfe7aa66257becc86f32850
Fulgurance <zohran.londais@gmail.com>
2024-06-24 11:21:57 +0100

    Updated virtualbox installer for tests

--- VirtualBox/7.0.18/7.0.18.cr
+++ VirtualBox/7.0.18/7.0.18.cr
@@ -13,15 +13,29 @@ class Target < ISM::Software
     def build
         super
 
-        runFile(file:   "kBuild/bin/linux.amd64/kmk",
-                path:   mainWorkDirectoryPath)
+        localConfigKmkData = <<-CODE
+        VBOX_PATH_APP_PRIVATE_ARCH := /usr/lib/virtualbox
+        VBOX_PATH_SHARED_LIBS := $(VBOX_PATH_APP_PRIVATE_ARCH)
+        VBOX_WITH_ORIGIN :=
+        VBOX_WITH_RUNPATH := $(VBOX_PATH_APP_PRIVATE_ARCH)
+        VBOX_PATH_APP_PRIVATE := /usr/share/virtualbox
+        VBOX_PATH_APP_DOCS := /usr/share/doc/virtualbox
+        VBOX_WITH_TESTCASES :=
+        VBOX_WITH_TESTSUITE :=
+        CODE
+        fileWriteData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/bashrc",localConfigKmkData)
+
+        runFile(file:                   "kBuild/bin/linux.amd64/kmk all",
+                path:                   mainWorkDirectoryPath,
+                environmentFilePath:    "#{mainWorkDirectoryPath}/env.sh")
+
+        #Then we need to build the modules !
     end
     
     def prepareInstallation
         super
 
         exit 1
-        #makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
     end
 
     def showInformations
